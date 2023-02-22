8680e115f0721a04ad84167463ffb06d31951b40
Jinjin <81174840+Poposong@users.noreply.github.com>
2023-02-22 23:09:19 +0900

    [Gold V] Title: 감시 피하기, Time: 88 ms, Memory: 12696 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -3,9 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-/*
 
-*/
 class Dot{
 	int x;
 	int y;
@@ -17,7 +15,9 @@ class Dot{
 		this.y=y;
 	}
 }
+
 public class Main {
+	
 	public static int N;
 	
 	public static char[][] map;
@@ -52,33 +52,39 @@ public class Main {
 			}
 		}
 		
-		obstacle(0);
+		obstacle(0, 0, 0);
 		
 		System.out.println(answer? "YES" : "NO");
 		
 	}
 	
-	static void obstacle(int count) {
-		if(count == 3) {
-			// 3개의 장애물을 설치함
-			if(isCheck()) { // 장애물을 설치할 수 있는 경우
-				answer = true;
-			}
+	// 3개의 장애물을 map[][] 에 설치하는 메소드
+	static void obstacle(int count, int r, int c) {
+		if(count == 3) { // 3개의 장애물을 설치함
 			
+			if(isCheck())  // 장애물을 설치할 수 있는 경우
+				answer = true;
+
 			return;
 		}
 		
-		for(int i = 0; i<N; i++) {
-			for(int j = 0; j<N; j++) {
+		for(int i = r; i<N; i++) {
+			int j;
+			if(i == r) 
+				j = c;
+			else
+				j = 0;
+			for(; j<N; j++) {
 				if(map[i][j] == 'X') {
 					map[i][j] = 'O';
-					obstacle(count+1);
+					obstacle(count+1, i, j);
 					map[i][j] = 'X';
 				}
 			}
 		}
 	}
 	
+	// 선생님(T) 이 상,하,좌,우 모두 체크해서 장애물이 나타나기 전에 학생을 발견하는지 체크하는 메소드
 	static boolean isCheck() {
 		for(Dot dot : teacher) {
 			for(int i =0; i<4; i++) {
@@ -102,6 +108,7 @@ public class Main {
 		return true;
 	}
 	
+	// x와 y의 좌표가 배열의 범위 안에 존재하는지 체크하는 메소드
 	static boolean rangeCheck(int x, int y) {
 		if(0<=x && x < N && 0 <= y && y < N)
 			return true;
