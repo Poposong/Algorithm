ff59b3f182f36b6d161199c40d777b1f80a5d158
jinniy <81174840+Poposong@users.noreply.github.com>
2023-08-30 22:01:26 +0900

    Update 두 원 사이의 정수 쌍.java

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -5,11 +5,17 @@ class Solution {
         long answer = 0;
 
         long num1, num2;
+        
+        answer += r2-r1+1;
 
-        for(int x = 1; x <= r2; x++){      
-            num1 = (int)Math.ceil(Math.sqrt(1.0*r1*r1 - 1.0*x*x));
-            num2 = (int)Math.floor(Math.sqrt(1.0*r2*r2 - 1.0*x*x));
-            answer += (num2-num1+1);
+        for(int x = 1; x < r2; x++){
+            if(x < r1){
+                num1 = (int)Math.ceil(Math.sqrt(1.0*r1*r1 - 1.0*x*x));
+                num2 = (int)Math.floor(Math.sqrt(1.0*r2*r2 - 1.0*x*x));
+                answer += (num2-num1+1);
+            }else{
+                answer += Math.floor(Math.sqrt(1.0*r2*r2 - 1.0*x*x));
+            }
         }
         
         return answer*4;
