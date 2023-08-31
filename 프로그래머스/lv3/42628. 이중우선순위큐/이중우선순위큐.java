b93d56df6c73f374f404b61e1c5ebc64cc62f341
jinniy <81174840+Poposong@users.noreply.github.com>
2023-08-31 17:23:01 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.92 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static List<Integer> data;
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        
+        // 오름차순 정렬
+        data = new LinkedList<>();
+        
+        String[] str;
+        int num;
+        for(String operation : operations){
+            str = operation.split(" ");
+            
+            num = Integer.parseInt(str[1]);
+            
+            //System.out.println(data);
+            
+            if(str[0].equals("I")){
+                data.add(num);
+            }else{ // D
+                if(data.isEmpty())
+                    continue;
+                reOrder();
+                if(num == -1){ // 최소를 구하기
+                    data.remove(0);
+                }else{ // 최대를 구하기
+                    data.remove(data.size()-1);
+                }
+            }
+        }
+        
+        //System.out.println(data);
+        
+        if(data.isEmpty()){
+            answer[0] = 0;
+            answer[1] = 0;
+        }else if(data.size() == 1){
+            answer[0] = answer[1] = data.get(0);
+        }else{
+            reOrder();
+            answer[0] = data.get(data.size()-1);
+            answer[1] = data.get(0);
+        }
+        
+        
+        
+        return answer;
+    }
+    
+    static void reOrder(){
+        Collections.sort(data, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer n1, Integer n2){
+                return n1-n2;
+            }
+        });
+    }
+}
