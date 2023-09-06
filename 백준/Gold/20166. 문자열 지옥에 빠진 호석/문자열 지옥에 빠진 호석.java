e8767917e223d98dd7f5be475d62d905ae5ddb4c
jinniy <81174840+Poposong@users.noreply.github.com>
2023-09-06 22:46:39 +0900

    Update 문자열 지옥에 빠진 호석.java

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
@@ -1,6 +1,4 @@
-/*
-수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다
- */
+package Algorithm;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
@@ -13,17 +11,11 @@ public class Main {
 
     static int dx[] = {-1, 0, 1, 0, -1, 1, 1, -1}; // 직선, 대각선
     static int dy[] = {0, -1, 0, 1, -1, -1, 1, 1};
-
     static char map[][];
- //   static Map<Integer, ArrayList<String>> findWords;
-
-    static Map<String, Integer> findCount;
-
     static char[] current;
     static int maxLen;
-
     static String[] order;
-    static HashMap<String, Integer> resultList;
+    static HashMap<String, Integer> resultCount;
 
     public static void main(String[] args) throws IOException {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,13 +26,6 @@ public class Main {
        K = Integer.parseInt(str[2]);
 
        map = new char[N][M];
-//       findWords = new HashMap<>();
-//       for(int i = 1; i<=5; i++){
-//           findWords.put(i, new ArrayList<String>());
-//       }
-
-        findCount = new HashMap<>();
-
 
        String s;
        for(int i = 0; i<N; i++){
@@ -50,66 +35,44 @@ public class Main {
             }
        }
 
-       resultList = new HashMap<>();
+       resultCount = new HashMap<>();
        order = new String[K];
 
        maxLen = -1;
        for(int i =0; i<K; i++){
            s = br.readLine();
-
-           resultList.put(s, 0);
            order[i] = s;
+           resultCount.put(s, 0);
            maxLen = Math.max(maxLen, s.length());
-
-           if(!findCount.containsKey(s)){
-               findCount.put(s, 1);
-           }else{
-              // findCount.getOrDefault(s, findCount.get(s) + 1);
-               int t = findCount.get(s);
-               findCount.put(s, t + 1);
-           }
        }
-
-
 
         current = new char[5];
 
-        //System.out.println("maxLen : "+maxLen);
-
         for(int i = 0; i<N; i++){
            for(int j = 0; j<M; j++){
-
                current[0] = map[i][j];
-               checkWord(0, i, j);
-
                dfs(1, i, j);
-             //  System.out.println("-----------");
            }
         }
 
         StringBuilder sb = new StringBuilder();
         for(String key : order){
-            sb.append(resultList.get(key)).append("\n");
+            sb.append(resultCount.get(key)).append("\n");
         }
         System.out.println(sb.toString());
-
     }
 
     static void dfs(int len, int x, int y){
-
         if(len >= maxLen){
             return;
         }
 
-        String str;
         int nx, ny;
         for(int i = 0; i<8; i++){
             nx = x + dx[i];
             ny = y + dy[i];
 
-          // System.out.println(nx+","+ny);
             if(rangeCheck(nx,ny)){
-
                 checkWord(len, nx, ny);
             }else{
                 if(nx < 0){
@@ -122,15 +85,11 @@ public class Main {
                 if(ny < 0){
                     ny = M-1;
                 }
-
                 if(M <= ny){
                     ny = 0;
                 }
-
                 checkWord(len, nx, ny);
             }
-
-          //  System.out.println("len : "+ len);
             dfs(len+1, nx, ny);
         }
     }
@@ -138,19 +97,16 @@ public class Main {
     static void checkWord(int len, int nx, int ny){
         current[len] = map[nx][ny];
         String str = transferStr(len+1);
-        int cnt = 0;
-        if(resultList.containsKey(str)){
-            cnt = findCount.get(str);
-            int t = resultList.get(str);
-            resultList.put(str, t + 1);
+        if(resultCount.containsKey(str)){
+            resultCount.put(str, resultCount.getOrDefault(str, 0) + 1);
         }
     }
+
     static String transferStr(int len){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i<len; i++){
             sb.append(current[i]);
         }
-
         return sb.toString();
     }
 
