4d988e7c58865f147391e8a23aeb18cd86b10343
jinniy <81174840+Poposong@users.noreply.github.com>
2023-09-08 22:14:13 +0900

    Update 여행 가자.java

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,91 +1,81 @@
-import java.awt.List;
+package Algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
+// 도시는 1부터 N까지 존재한다.
 
 public class Main {
-	
-	public static int N,M;
-	
-	public static ArrayList[] graph;
-	
-	public static boolean[][] visited;
-	
-	public static int count;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		
-		String[] str;
-		
-		graph = new ArrayList[N+1];
-		
-		for(int i =0; i<N+1; i++)
-			graph[i] = new ArrayList<Integer>();
-		
-		for(int i = 1; i<N+1; i++) {
-			str = br.readLine().split(" ");
-			for(int j = 0; j<str.length; j++) {
-				if(str[j].equals("1")) {
-					graph[i].add(j+1);
-				}
-			}
-		}
-		
-	
-		
-		
-		visited = new boolean[N+1][2];
-		
-		count = 0;
-		
-		int num = 0;
-		str = br.readLine().split(" ");
-		for(int i = 0; i<M; i++) {
-			num = Integer.parseInt(str[i]);
-			if(!visited[num][0])
-				count++;
-			visited[num][0] = true; 
-		}
-		
-		bfs(num);
-		System.out.println(count==0? "YES" : "NO");
-	}
-	
-	static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		
-		queue.offer(start);
-		visited[start][0] = false;
-		visited[start][1] = true;
-		
-		count--;
-		int number;
-		while(!queue.isEmpty()) {
-			number = queue.poll();
-		
-			int value;
-			for(int i =0; i<graph[number].size(); i++) {
-				value = (int) graph[number].get(i);
-				if(!visited[value][1]) {
-					if(visited[value][0]) {
-						visited[value][0] = false;
-						count--;
-					}
-					visited[value][1] = true;
-					queue.add(value);
-				}
-			}
-			
+    static int N, M; // 도시의 수, 여행 계획에 속한 도시들의 수
+    static HashMap<Integer, ArrayList<Integer>> graph;
+    static boolean[] seat;
+    static boolean[] visited;
+    static int count;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-		}
-	}
+        seat = new boolean[N+1];
+        visited = new boolean[N+1];
+
+        graph = new HashMap<>();
+        for(int i = 0; i<N+1; i++){
+            graph.put(i, new ArrayList<Integer>());
+        }
+
+        String[] str;
+        for(int i = 1; i<N+1; i++){
+            str = br.readLine().split(" ");
+            for(int j = 0; j<N; j++){
+                if(Integer.parseInt(str[j]) == 1){
+                    graph.get(i).add(j+1);
+                }
+            }
+        }
+
+        str = br.readLine().split(" ");
+        int num = 0;
+        for(String s : str){
+            num = Integer.parseInt(s);
+            if(!seat[num]){
+                seat[num] = true;
+                count++;
+            }
+        }
+
+        bfs(num);
+
+        if(count == 0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+
+    static void bfs(int start){
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        visited[start] = true;
+        count--;
+
+        int vertex;
+        while(!queue.isEmpty()){
+            vertex = queue.poll();
+
+            for(int value : graph.get(vertex)){
+                if(!visited[value]){
+                    if(seat[value]){
+                        seat[value] = false;
+                        count--;
+                    }
+                    visited[value] = true;
+                    queue.offer(value);
+                }
+            }
+        }
+    }
+
 }
