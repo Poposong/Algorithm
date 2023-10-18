b9861f2e93dcc4d5eaaa2cbc6bbeb57a85c96f0e
jinniy <81174840+Poposong@users.noreply.github.com>
2023-10-19 08:31:16 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 192 ms, Memory: 74436 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -1,21 +1,30 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
+	static String s1, s2;
+	static int[][] map;
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String cmp1 = br.readLine();
-		String cmp2 = br.readLine();
-		int len1 = cmp1.length();
-		int len2 = cmp2.length();
-		int max = 0;
-		int[][] map = new int[len1 + 1][len2 + 1];
-		for(int i = 1; i <= len1; i++) {
-			for(int j = 1; j <= len2; j++) {
-				if(cmp1.charAt(i-1) == cmp2.charAt(j-1)) {
-					map[i][j] = map[i-1][j-1] + 1;
+		s1 = br.readLine();
+		s2 = br.readLine();
+		
+		map = new int[s1.length()+1][s2.length()+1];
+		
+		int max= 0;
+		for(int i = 1; i<=s1.length(); i++) {
+			for(int j = 1; j<=s2.length(); j++) {
+				if(s1.charAt(i-1) == s2.charAt(j-1)) {
+					map[i][j] = map[i-1][j-1]+1;
 					max = Math.max(max, map[i][j]);
 				}
 			}
 		}
 		System.out.println(max);
+		
+		
 	}
+	
+
 }
