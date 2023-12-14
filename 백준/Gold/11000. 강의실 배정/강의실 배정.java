b09ee288a7409ed7166f613a4e404c688aaa9df7
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-14 10:48:14 +0900

    Update 강의실 배정.java

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,10 +1,17 @@
+package Baekjoon;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+// 시간복잡도
+// Arrays.sort의 시간복잡도 = O(nlogn)
+// O(nlogn) + O(n * (logn + logn)) = 200,000*18 + 200,000*2*18 = 3,600,000 + 7,200,000 = 10,800,000 < 1억
+
+public class BJ_11000_강의실_배정 {
     static int N;
 
     public static void main(String[] args) throws IOException {
+        System.out.println(Math.pow(2, 18));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         int[][] ban = new int[N][2];
@@ -42,9 +49,9 @@ public class Main {
         for(int i = 1; i<ban.length; i++){
             end = pq.peek();
             if(end <= ban[i][0]){
-                pq.poll();
+                pq.poll(); // 삭제 : logn
             }
-            pq.offer(ban[i][1]);
+            pq.offer(ban[i][1]); // 삽입 : logn
         }
 
         return pq.size();
