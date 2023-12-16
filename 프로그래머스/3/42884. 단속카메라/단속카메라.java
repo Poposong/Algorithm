9a494543aef58e70cc5557b9526054c0bdd80075
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-16 19:34:37 +0900

    [level 3] Title: 단속카메라, Time: 7.15 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        
+        if(routes.length == 1)
+            return 1;
+        
+        Arrays.sort(routes, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                if(a[0] == b[0]){
+                    return a[1]-b[1]; // 오름차순
+                }
+                return a[0]-b[0]; // 오름차순
+            }
+        });
+        //System.out.println(r1+","+r2);
+        
+        int r1 = routes[0][0], r2 = routes[0][1];
+        int n1, n2;
+        for(int i = 1; i<routes.length; i++){
+            n1 = Math.max(r1, routes[i][0]);
+            n2 = Math.min(r2, routes[i][1]);
+            if(n1 <= n2){
+                r1 = n1;
+                r2 = n2;
+            }else{
+                answer++;
+                r1 = routes[i][0];
+                r2 = routes[i][1];
+            }
+          //  System.out.println(r1+","+r2+",");
+        }
+        
+        return answer+1;
+    }
+}
