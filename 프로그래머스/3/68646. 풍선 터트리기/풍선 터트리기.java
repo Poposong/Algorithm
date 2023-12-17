bb62bb3be685c9d11e521a732204f2b1e67f2aa8
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-17 16:20:58 +0900

    [level 3] Title: 풍선 터트리기, Time: 21.28 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -9,14 +9,14 @@ class Solution {
         int[] right = new int[len];
         
         left[0] = a[0];
-        for(int i = 1; i<len; i++){
+        right[len-1] = a[len-1];
+        
+        for(int i = 1, j = len-2; i<len && j>=0 ; i++, j--){
             left[i] = Math.min(left[i-1], a[i]);
+            right[j] = Math.min(right[j+1], a[j]);
         }
         
-        right[len-1] = a[len-1];
-        for(int i = len-2; i>=0; i--){
-            right[i] = Math.min(right[i+1], a[i]);
-        }
+    
         
         for(int i = 0; i<len; i++){
             if(!(left[i] < a[i] && a[i] > right[i])){
