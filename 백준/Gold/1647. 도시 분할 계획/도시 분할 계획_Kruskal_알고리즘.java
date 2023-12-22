edcd4d59ac07382d00b49837002579614f2b3441
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-22 11:34:26 +0900

    Update 도시 분할 계획_Kruskal_알고리즘.java

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획_Kruskal_알고리즘.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획_Kruskal_알고리즘.java
@@ -1,3 +1,4 @@
+package Baekjoon;
 import java.io.*;
 import java.util.*;
 
@@ -7,79 +8,71 @@ import java.util.*;
 2. 간선을 하나씩 뽑으면서 사이클이 생기는지 확인한다(union-find를 사용하여 부모를 확인한다)
 3. 뽑은 간선의 수가 정점의 수보다 하나 작을 때까지 반복한다.
 **/
-public class Main {
+public class BJ_1647_도시분할계획_kruskal알고리즘 {
     static int N, M;
-    static class Node{
-        int v;
-        int w;
+    static class Edge{
+        int a;
+        int b;
+        int c;
 
-        public Node(int v, int w){
-            this.v = v;
-            this.w = w;
+        public Edge(int a, int b, int c){
+            this.a=a;
+            this.b=b;
+            this.c=c;
         }
     }
 
-    static boolean[] visited;
-    static List[] edgeList;
-
+    static Edge[] edgeList;
+    static int[] parent;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str = br.readLine().split(" ");
         N = Integer.parseInt(str[0]);
         M = Integer.parseInt(str[1]);
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>(){
-            public int compare(Node n1, Node n2){
-                return n1.w-n2.w;
-            }
-        });
-        
-        int a, b, c;
-        edgeList = new ArrayList[N+1];
-        for(int i = 0; i<N+1; i++){
-            edgeList[i] = new ArrayList<Node>();
-        }
-
+        edgeList = new Edge[M];
         for(int i =0; i<M; i++){
             str = br.readLine().split(" ");
-            a = Integer.parseInt(str[0]);
-            b = Integer.parseInt(str[1]);
-            c = Integer.parseInt(str[2]);
-            edgeList[a].add(new Node(b, c));
-            edgeList[b].add(new Node(a, c));
-        }
-        // 시작할 정점의 간선들을 우선순위 큐에 넣는다.
-        for(int i = 0; i<edgeList[1].size(); i++){
-            Node node = (Node)edgeList[1].get(i);
-            pq.offer(node);
+            edgeList[i] = new Edge(Integer.parseInt(str[0]), Integer.parseInt(str[1]), Integer.parseInt(str[2]));
         }
 
-        visited=new boolean[N+1];
-        visited[1] = true;
-        Node node;
-        int sum = 0, max = 0, cnt = 0;
-        while(!pq.isEmpty()){
-            node = pq.poll();
-            if(!visited[node.v]){
-                for(int i = 0; i<edgeList[node.v].size(); i++){
-                    Node tempNode = (Node)edgeList[node.v].get(i);
-                    if(!visited[tempNode.v]){
-                        pq.offer(tempNode);
-                    }
+        Arrays.sort(edgeList, new Comparator<Edge>(){
+           public int compare(Edge e1, Edge e2){
+               return e1.c-e2.c;
+           }
+        });
+
+        parent=new int[N+1];
+        for(int i = 0; i<N+1; i++){
+            parent[i]=i;
+        }
+
+        int p1, p2, sum = 0, max = 0, cnt = 0;
+        for(Edge edge:edgeList){
+            p1 = findParent(edge.a);
+            p2 = findParent(edge.b);
+            if(p1!=p2){
+                if(p1<p2){
+                    parent[p2] = p1;
+                }else{
+                    parent[p1] = p2;
                 }
-                sum += node.w;
-                max = Math.max(max, node.w);
                 cnt++;
-                visited[node.v] = true;
+                sum += edge.c;
+                max = Math.max(max, edge.c);
             }
-
             if(cnt == N-1){
                 break;
             }
         }
-
-
         System.out.println(sum-max);
+    }
+
+    static int findParent(int idx){
+        if(idx == parent[idx]){
+            return parent[idx];
+        }
+        return findParent(parent[idx]);
     }
 
 
