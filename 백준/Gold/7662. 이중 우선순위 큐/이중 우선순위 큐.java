1052ea4b5336e683fe5f61fae947e8a420ab8f88
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-23 15:08:36 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2856 ms, Memory: 431404 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,77 +1,60 @@
-package Baekjoon;
 import java.io.*;
 import java.util.*;
 
 /**
- * 처음에는 리스트로 구현해서 삽입하는 경우에만 이분탐색을 하려고 했지만 이분탐색은 최악인 경우 O(N)의 시간복잡도를 가지기 때문에 다른 방법으로 접근했다.
- * 최소 우선순위 큐와 최대 우선순위 큐로 구현했을 때 삭제하는 경우는 O(logN)이다
+TreeMap
  * */
 
-public class BJ_7662_이중우선순위큐 {
+public class Main {
 
-    static Map<Integer, Integer> count;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         int k, value;
 
-        PriorityQueue<Integer> minOrder = new PriorityQueue<>();
-        PriorityQueue<Integer> maxOrder = new PriorityQueue<>(Collections.reverseOrder());
-        count = new HashMap<>();
+        TreeMap<Integer, Integer> pq = new TreeMap<>();
 
         String[] str;
         while(t-- != 0){
-            minOrder.clear();
-            maxOrder.clear();
-            count.clear();
+            pq.clear();
             k = Integer.parseInt(br.readLine());
             for(int i = 0; i<k; i++){
                 str = br.readLine().split(" ");
                 value = Integer.parseInt(str[1]);
                 switch(str[0].charAt(0)){
                     case 'I':
-                        minOrder.offer(value); // O(logN)
-                        maxOrder.offer(value); // O(logN)
-                        count.put(value, count.getOrDefault(value, 0)+1);
+                        pq.put(value, pq.getOrDefault(value, 0)+1);
                         break;
                     case 'D':
-                        if(count.size()!=0){
-                            if(value == -1) {
-                                removeValue(minOrder); // O(logN)
-                            }else{
-                                removeValue(maxOrder); // O(logN)
-                            }
+                        if(pq.size()==0) continue;
+
+                        int key;
+                        if(value == -1) {
+                            key = pq.firstKey();
+                        }else{
+                            key = pq.lastKey();
                         }
+
+                        int cnt = pq.get(key);
+
+                        if(cnt != 1){
+                            pq.put(key, cnt-1);
+                        }else{
+                            pq.remove(key);
+                        }
+
                         break;
                 }
             }
 
-            if(count.size()==0){
+            if(pq.size()==0){
                 sb.append("EMPTY\n");
             }else{
-                int max = removeValue(maxOrder);
-                sb.append(max).append(" ").append(count.size()==0? max : removeValue(minOrder)).append("\n");
+                sb.append(pq.lastKey()).append(" ").append(pq.firstKey()).append("\n");
             }
         }
         System.out.println(sb.toString());
     }
 
-    static int removeValue(PriorityQueue<Integer> pq){
-        int num;
-        while(true){ // O(logN) + O(log(N-1)) + O(log(N-2)) + ... + O(log(1)) === O(log(N))
-           num = pq.poll();
-
-            int cnt = count.getOrDefault(num, 0);
-            if(cnt == 0) continue;
-
-           if(cnt == 1){
-               count.remove(num);
-           }else{
-               count.put(num, cnt-1);
-           }
-           break;
-        }
-        return num;
-    }
 }
