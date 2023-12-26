c140a1469c97986c7b58dcf2714187c4ff487c43
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-26 21:27:09 +0900

    Update 양과 늑대.java

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -24,7 +24,7 @@ class Solution {
     static void dfs(int sheep, int wolf, int curNode, int[] info, ArrayList<Integer> path){
         if(info[curNode] == 0){ // 양
             sheep++;
-        }else{
+        }else{ // 늑대
             wolf++;
         }
         
@@ -33,11 +33,11 @@ class Solution {
         maxSheep = Math.max(maxSheep, sheep);
         
         ArrayList<Integer> copyPath = new ArrayList<>(path);
-        for(int vertex : graph.get(curNode)){
+        for(int vertex : graph.get(curNode)){ // 현재 노드가 갈 수 있는 자식 노드들을 copyPath 리스트에 넣어준다.
             copyPath.add(vertex);
         }
-        copyPath.remove(Integer.valueOf(curNode));
-        for(int nextNode : copyPath){
+        copyPath.remove(Integer.valueOf(curNode)); // 부모 노드에서 자식 노드로 가는 경우는 체크했으므로 제외시킨다.
+        for(int nextNode : copyPath){ // 자식 노드와 형제 노드 또는 상속 받은 노드들로부터 갈 수 있는 경우를 모두 체크한다.
             dfs(sheep, wolf, nextNode, info, copyPath);
         }
     }
