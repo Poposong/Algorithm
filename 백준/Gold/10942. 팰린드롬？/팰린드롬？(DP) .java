b28c77804f49e45cb519ed3478956277b1e41f22
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-31 12:24:33 +0900

    Update 팰린드롬？(DP) .java

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？(DP) .java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？(DP) .java
@@ -41,13 +41,6 @@ public class Main {
             sb.append(dp[Integer.parseInt(str[0])][Integer.parseInt(str[1])] ? 1 : 0).append("\n");
         }
         System.out.println(sb.toString());
-//
-//        for(int i = 1; i<N+1; i++){
-//            for(int j = 1; j<N+1; j++){
-//                System.out.print(dp[i][j]+" ");
-//            }
-//            System.out.println();
-//        }
 
     }
 }
