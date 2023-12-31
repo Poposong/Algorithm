c976ec0eb90273060a7832be051d9ca323ec40b2
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-31 13:31:22 +0900

    Update 보행자 천국.java

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -35,13 +35,6 @@ class Solution {
             }
         }
         
-        // for(int i = 1; i<m+1; i++){
-        //     for(int j = 1; j<n+1 ;j++){
-        //         System.out.print(dp[0][i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        
         return dp[0][m][n];
     }
     
