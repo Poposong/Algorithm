acee05e2b8fdb63e0f66f6aabe267b8040c777f5
jinniy <81174840+Poposong@users.noreply.github.com>
2023-12-31 14:02:19 +0900

    Update 주문량이 많은 아이스크림들 조회하기.sql

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -5,3 +5,12 @@ where f.flavor = j.flavor
 group by f.flavor
 order by sum(f.total_order) - f.total_order * (count(f.total_order) - 1) + sum(j.total_order) desc
 limit 3;
+
+
+-- 코드를 입력하세요
+SELECT f.flavor
+from FIRST_HALF f, JULY j
+where f.flavor = j.flavor
+group by f.flavor
+order by sum(f.total_order) + sum(j.total_order) desc
+limit 3;
