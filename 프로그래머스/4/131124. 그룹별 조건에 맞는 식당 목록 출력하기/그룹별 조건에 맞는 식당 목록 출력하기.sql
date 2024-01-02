5bc6c5d3afac538cae097b889c8aa5031996e77f
jinniy <81174840+Poposong@users.noreply.github.com>
2024-01-02 11:50:59 +0900

    Update 그룹별 조건에 맞는 식당 목록 출력하기.sql

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,23 +1,4 @@
--- 코드를 입력하세요
--- 가장 큰 리뷰 값 -> 해당하는 회원들 -> 
-# SELECT m.member_id, count(m.member_id)
-# from member_profile m, rest_review r
-# where m.member_id = r.member_id
-# group by member_id
-# having count(m.member_id) = (
-#     SELECT count(mp.member_id)
-#     from member_profile mp, rest_review rr
-#     where mp.member_id = rr.member_id
-#     group by mp.member_id
-#     order by count(mp.member_id) desc limit 1
-# );
-
-# SELECT m.member_id, count(m.member_id)
-# from member_profile m, rest_review r
-# where m.member_id = r.member_id
-# group by member_id
-# order by count(m.member_id) desc;
-
+-- (1)
 select m.member_name, r.review_text, date_format(r.review_date, '%Y-%m-%d')
 from member_profile m, rest_review r
 where m.member_id = r.member_id
@@ -35,3 +16,15 @@ and m.member_id in(
     )
 )
 order by r.review_date, r.review_text;
+
+
+-- (2)
+select T.member_name, B.review_text, date_format(B.review_date, '%Y-%m-%d') as review_date 
+from (select R.MEMBER_ID, M.MEMBER_NAME, rank() over(order by `CNT` desc) as ranking
+        from (select *, count(member_id) as CNT
+        from rest_review
+        group by member_id) as R, member_profile M
+        where R.member_id = M.member_id) as T, rest_review B
+where T.member_id = B.member_id and T.ranking = 1
+order by B.review_date, B.review_text;
+
