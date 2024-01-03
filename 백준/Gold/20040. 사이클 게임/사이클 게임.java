9e9e77f000525e082ea658cf058015dbe023b60f
jinniy <81174840+Poposong@users.noreply.github.com>
2024-01-03 10:16:42 +0900

    Update 사이클 게임.java

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,6 +1,7 @@
+package Baekjoon;
 import java.io.*;
 import java.util.*;
-public class Main {
+public class BJ_20040_사이클게임 {
     static int[] number;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,29 +13,39 @@ public class Main {
         for(int i = 0; i<N; i++){
             number[i] = i;
         }
-
+        
+        // 시간복잡도 : O(M)
         int v1,v2,p1,p2;
         for(int i = 1; i<=M; i++){
             str = br.readLine().split(" ");
             v1 = Integer.parseInt(str[0]);
             v2 = Integer.parseInt(str[1]);
-
-            p1 = findParent(v1);
-            p2 = findParent(v2);
-
-            if(p1 == p2){
+            
+            
+            if(!union(v1, v2)){
                 System.out.println(i);
                 return;
             }
-
-            if(p1 < p2){
-                number[p2] = p1;
-            }else{
-                number[p1] = p2;
-            }
         }
-        // System.out.println(Arrays.toString(number));
+
         System.out.println(0);
+    }
+    // 합치기
+    public static boolean union(int v1, int v2){
+        int p1 = findParent(v1);
+        int p2 = findParent(v2);
+
+        if(p1 == p2){ // 사이클이 발생함
+            return false;
+        }
+
+        if(p1 < p2){
+            number[p2] = p1;
+        }else{
+            number[p1] = p2;
+        }
+
+        return true;
     }
 
     // 부모를 찾음
@@ -42,7 +53,7 @@ public class Main {
         if(number[vertex] == vertex){
             return vertex;
         }
-        return number[vertex] = findParent(number[vertex]);
+        return number[vertex] = findParent(number[vertex]); // 경로압축(트리의 높이를 낮춘다)
     }
 
 }
