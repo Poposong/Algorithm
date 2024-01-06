e5927e8ffb064054827e23d1e3e01d11009820c6
jinniy <81174840+Poposong@users.noreply.github.com>
2024-01-06 12:09:29 +0900

    Update 카드 섞기.java

--- 백준/Gold/1091. 카드 섞기/카드 섞기.java
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.java
@@ -11,13 +11,12 @@ public class Main {
         S = new int[N];
         String[] str1 = br.readLine().split(" ");
         String[] str2 = br.readLine().split(" ");
-
         int cnt = 0;
         for(int i = 0; i<N; i++){
             P[i] = Integer.parseInt(str1[i]);
             S[i] = Integer.parseInt(str2[i]);
             cards[i] = i;
-            if(i%3 == P[i]){
+            if(i%3 == P[i]){ // 카드를 처음부터 섞지 않아도 돌아가는 경우
                 cnt++;
             }
         }
@@ -27,46 +26,38 @@ public class Main {
             return;
         }
 
-
-
         int[] temp = new int[N];
+        int sameCnt, recurCnt;
         cnt=0;
         while(true){
-
-            int sameCheck = 0;
-            int recursive = 0;
+            sameCnt = 0;
+            recurCnt = 0;
             for(int i = 0; i<N; i++){
-
-                int idx = S[i];
-                int value = cards[i];
                 temp[S[i]] = cards[i];
-                if(S[i] == temp[S[i]]){
-                    recursive++;
+                if(S[i] == temp[S[i]]){ // 사이클을 체크한다.
+                    recurCnt++;
                 }
-
-                if(P[cards[i]] == S[i]%3){
-                    sameCheck++;
+                if(P[cards[i]] == S[i]%3){ // 카드를 올바르게 섞었는지 체크한다.
+                    sameCnt++;
                 }
             }
 
             cnt++;
 
-            if(sameCheck == N){
+            if(sameCnt == N){
                 System.out.println(cnt);
                 return;
             }
 
-            if(recursive == N){
+            if(recurCnt == N){
                 System.out.println(-1);
                 return;
             }
 
 
-            for(int i = 0; i<N; i++){
+            for(int i = 0; i<N; i++){ // 섞은 카드를 복사한다.
                 cards[i] = temp[i];
             }
-
-//            System.out.println(Arrays.toString(cards));
         }
 
     }
