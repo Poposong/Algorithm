c84e5e8a45ad0001e37c39efbfaa407471def31b
jinniy <81174840+Poposong@users.noreply.github.com>
2024-01-09 10:59:25 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.26 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/**
+n : 1~60,000까지의 자연수
+*/
+
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i<n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        }
+        
+        return dp[n-1];
+    }
+}
