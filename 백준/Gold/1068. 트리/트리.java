9044bcb16944681113516c32e9ea342a1801f5ca
jinniy <81174840+Poposong@users.noreply.github.com>
2024-01-15 12:00:31 +0900

    [Gold V] Title: 트리, Time: 76 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -2,18 +2,18 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static int N, answer;
-
-    static boolean[] leafNodeCheck;
     static Map<Integer, ArrayList<Integer>> tree;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        answer = N;
-        tree = new HashMap<>();
-        for(int i = 0; i<N; i++){ // 부모노드와 자식노드들을 저장할 맵
+        answer = N; // 리프노드로 가능한 정점의 수
+        
+        tree = new HashMap<>(); // / 부모노드와 자식노드들을 저장할 맵
+        for(int i = 0; i<N; i++){ 
             tree.put(i, new ArrayList<Integer>());
         }
-        leafNodeCheck = new boolean[N]; // 리프노드를 체크하기 위한 배열
+        
+        boolean[] leafNodeCheck = new boolean[N]; // 리프노드를 체크하기 위한 배열
         Arrays.fill(leafNodeCheck, false);
 
         int rootNode = 0;
@@ -32,33 +32,27 @@ public class Main {
                 answer--;
             }
         }
-
-       // System.out.println(answer);
-
-     //   System.out.println(Arrays.toString(leafNodeCheck));
+        // answer의 수가 현재까지 존재하는 리프노드의 수이다.
 
         int removeNode = Integer.parseInt(br.readLine());
-      //  System.out.println(removeNode+","+rootNode);
-        if(removeNode == rootNode){
+        if(removeNode == rootNode){ // 루트노드가 지우고 싶은 노드이면 리프노드는 0이다.
             System.out.println(0);
             return;
         }
 
         removeNode(removeNode);
 
-        int num = Integer.parseInt(str[removeNode]);
+        int num = Integer.parseInt(str[removeNode]); // 지웠을 때 removeNode의 상위 노드가 리프 노드가 되었을 경우를 체크한다.
         if(num != -1 && tree.get(num).size()== 1){
             answer++;
         }
 
-
-
-        System.out.println(answer == 0 ? 1 : answer);
+        System.out.println(answer);
 
     }
 
     static void removeNode(int vertex){
-        if(tree.get(vertex).size() == 0){
+        if(tree.get(vertex).size() == 0){ // 리프노드가 하나만 있었던 경우에는 지워야 하기 때문에 for문 밖에서 처리한다.
             answer--;
             return;
         }
