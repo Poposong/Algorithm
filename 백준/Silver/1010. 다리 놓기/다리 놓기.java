bde956fc5b10ef2850ae24514981ac09a49e3c2d
jinniy <81174840+Poposong@users.noreply.github.com>
2024-01-22 15:22:51 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 14748 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,41 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/*
- * dp 풀이
- */
+
 public class Main {
 
-	static int[][] dp = new int[31][31];
-
-	static int combi(int n, int r) {
-		if (dp[n][r] > 0) {
-			return dp[n][r];
-		}
-		if (n == r || r == 0) {
-			return 1;
-		}
-
-		return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
-	}
-
+	
+	public static int N,M, dp[][] = new int[30][30];
+	
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+	
 		int T = Integer.parseInt(br.readLine());
-
-		for (int tc = 0; tc < T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-
-			sb.append(combi(M, N) + "\n");
+		String str[];
+		
+		while(T--!=0) {
+			str = br.readLine().split(" ");
+			N = Integer.parseInt(str[0]);
+			M = Integer.parseInt(str[1]);
+			sb.append(combi(M,N)+"\n");
 		}
-		System.out.println(sb);
-
+		System.out.println(sb.toString());
+		
+		
 	}
+	static int combi(int n, int r) {
+		
+		if(dp[n][r]>0)
+			return dp[n][r];
+		
+		if(r == n || r == 0)
+			return dp[n][r] = 1;
+		
+		return dp[n][r]= combi(n-1,r-1) + combi(n-1,r);
+	}
+
+	
 
 }
